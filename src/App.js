6d52476178653a5bbf4335ca5b437edb523b1659
Steven Woods <steven.w89@hotmail.co.uk>
2020-05-07 22:02:32 +0100

    Add fetch function

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const imdb = require('imdb-api')
+
+function fetch() {
+  imdb.get({name: 'The Toxic Avenger'}, {apiKey: 'foo', timeout: 30000}).then(console.log).catch(console.log);
+}
+
 function App() {
+  fetch()
   return (
     <div className="App">
       <header className="App-header">
